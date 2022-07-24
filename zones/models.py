55c735a756e77f090cfc595b72efb926a5f821a8
Andrei Lushpanov <a.lushpanov@wis.software>
2022-07-24 16:50:32 +0300

    rename Zone's poly to area

--- zones/models.py
+++ zones/models.py
@@ -5,7 +5,7 @@ from django.contrib.gis.db import models
 
 class Zone(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    poly = models.PolygonField()
+    area = models.PolygonField()
 
     def __str__(self):
         return f'{id}'
