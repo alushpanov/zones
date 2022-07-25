ac56fbde4e3e69f45d66c5b48f495dbc5451e4d0
Andrei Lushpanov <a.lushpanov@wis.software>
2022-07-25 11:51:32 +0300

    coordinates serializer altered

--- zones/api/serializers.py
+++ zones/api/serializers.py
@@ -1,8 +1,12 @@
 import logging
 
-from rest_framework import serializers
+from collections import OrderedDict
 
-from zones.api.fields import CoordinatesField
+from django.contrib.gis.geos import Polygon
+
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from zones.models import Zone
 
 logger = logging.getLogger(__name__)
@@ -11,7 +15,29 @@ logger = logging.getLogger(__name__)
 class ZoneSerializer(serializers.Serializer):
 
     id = serializers.UUIDField(read_only=True)
-    coordinates = CoordinatesField()
+    coordinates = serializers.ListField(
+        child=serializers.ListField(
+            child=serializers.FloatField(),
+            min_length=2,
+            max_length=2,
+        ),
+        min_length=4
+    )
+
+    def to_representation(self, instance):
+        return OrderedDict(
+            id=instance.id,
+            coordinates=instance.coordinates.coords[0],
+        )
+
+    def validate_coordinates(self, value):
+        if value[0] != value[-1]:
+            raise ValidationError(
+                'The first and the last coordinates must be the same point in order to close the zone'
+            )
+        return value
 
     def create(self, validated_data):
-        return Zone.objects.create(coordinates=validated_data['coordinates'])
+        return Zone.objects.create(
+            coordinates=Polygon(validated_data['coordinates'])
+        )
